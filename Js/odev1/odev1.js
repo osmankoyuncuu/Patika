3fffdb9233010a11d3eea5a9aed3c3d49ef60767
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-09-06 23:23:46 +0300

    finish

--- Js/odev1/odev1.js
+++ Js/odev1/odev1.js
@@ -4,4 +4,29 @@ let userName = document.querySelector(`#userName`);
 
 userName.innerHTML = `${name1[0].toUpperCase()}${name1.slice(1).toLowerCase()}`;
 
-let clock = new Date();
+function clock() {
+  let hours = document.querySelector(`#hours`);
+  let minutes = document.querySelector(`#minutes`);
+  let seconds = document.querySelector(`#seconds`);
+  let day = document.querySelector(`#days`);
+  let days = [
+    "Pazar",
+    "Pazartesi",
+    "Salı",
+    "Çarşamba",
+    "Perşembe",
+    "Cuma",
+    "Cumartesi",
+  ];
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let d = new Date().getDay();
+
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+  day.innerHTML = days[d];
+}
+
+let interval = setInterval(clock, 100);
